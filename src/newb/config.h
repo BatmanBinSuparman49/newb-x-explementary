2c5bcc2ec9e89dae6feb49d48f7a36b6862f6364
Batman Bin Suparman <187012197+BatmanBinSuparman49@users.noreply.github.com>
2026-02-13 22:45:25 +0600

    Increase gamma and brightness settings in config

--- src/newb/config.h
+++ src/newb/config.h
@@ -29,7 +29,7 @@
 
 /* Color correction */
 #define NL_TONEMAP_TYPE 9              // 1:Exponential, 2:Reinhard, 3:Extended Reinhard, 4:ACES 5:John Habel's filmic (used in BSBE) 
-#define NL_GAMMA 1.2                  // 0.3 low ~ 2.0 high
+#define NL_GAMMA 1.4                  // 0.3 low ~ 2.0 high
 //#define NL_EXPOSURE 1.3              // [toggle] 0.5 dark ~ 3.0 bright
 #define NL_SATURATION 0.9            // [toggle] 0.0 grayscale ~ 4.0 super saturated
 //#define NL_TINT                      // [toggle] enable light/dark tone tinting
@@ -38,9 +38,9 @@
 
 /* Terrain lighting */
 #define NL_SUN_INTENSITY 3.8   // 0.5 weak ~ 5.0 bright
-#define NL_TORCH_INTENSITY 1.1  // 0.5 weak ~ 3.0 bright
+#define NL_TORCH_INTENSITY 1.2  // 0.5 weak ~ 3.0 bright
 #define NL_NIGHT_BRIGHTNESS 0.0 // 0.0 dark ~ 2.0 bright
-#define NL_CAVE_BRIGHTNESS 1.2  // 0.0 dark ~ 2.0 bright
+#define NL_CAVE_BRIGHTNESS 1.8  // 0.0 dark ~ 2.0 bright
 #define NL_SHADOW_INTENSITY 0.2 // 0.0 no shadow ~ 1.0 strong shadow
 //#define NL_BLINKING_TORCH     // [toggle] flickering light
 //#define NL_CLOUD_SHADOW       // [toggle] cloud shadow (simple clouds only)
